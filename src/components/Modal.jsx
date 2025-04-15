6ad0efd927c42284d0cb92e5bc23d6628f89a8f9
JoeyZ56 <jzazzi56@gmail.com>
2025-04-14 23:19:22 -0700

    created tailwind for maodel and reformated layout

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,23 @@
-import styles from "./Modal.module.scss";
-
+"use client";
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className={styles.overlay} onClick={onClose}>
-      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-        <button className={styles.closeButton} onClick={onClose}>
+    <div
+      onClick={onClose}
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="relative w-full max-w-lg p-6 bg-white rounded-lg shadow-lg"
+      >
+        <button
+          onClick={onClose}
+          className="absolute text-2xl font-bold text-gray-400 top-2 right-4 hover:text-gray-600"
+        >
           &times;
         </button>
-        <div className={styles.content}>{children}</div>
+        <div className="mt-4">{children}</div>
       </div>
     </div>
   );
